02f322330aecd93d9ce454b4e2c912c77a2dcdc5
Frxddy03 <Freddytomala09@gmail.com>
2024-07-04 16:40:17 -0500

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -1,16 +1,7 @@
-calcular_determinante <- function() {
-  cat("Ingrese los elementos de la matriz 2x2:\n")
-  a <- as.numeric(readline(prompt = "Ingrese el elemento a11: "))
-  b <- as.numeric(readline(prompt = "Ingrese el elemento a12: "))
-  c <- as.numeric(readline(prompt = "Ingrese el elemento a21: "))
-  d <- as.numeric(readline(prompt = "Ingrese el elemento a22: "))
-
-  matriz <- matrix(c(a, b, c, d), nrow = 2, byrow = TRUE)
-  cat("\nMatriz ingresada:\n")
-  print(matriz)
-
+calcular_determinante <- function(a, b, c, d) {
+  # Calculamos el determinante
   det <- a * d - b * c
-  cat("\nEl determinante de la matriz es:", det, "\n")
+  return(det)
 }
 
 
